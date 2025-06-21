2974a03d2f2f1dab53236a08f14d62ad4a2d8fad
rahul-singhania223 <rahulsinghania406@gmail.com>
2025-06-21 16:09:41 +0530

    testing

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ def process(input):
     job_input = input["input"]
     image_url = job_input["image_url"]
 
-    return image_url
+    result_image = {
+        "image_url": "image_url",
+        "filename": "filename"
+    }
+
+    return result_image
 
 
 def main():
